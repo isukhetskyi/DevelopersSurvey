0e420833359492f3e4c89887ff9dd7dac3f8757a
isukhetskyi <ivan.sukhetskyi@gmail.com>
2017-06-12 08:57:44 +0300

    Started doing survey form

---
+++ DevelopersSurvey.Web/UI/Survey/Components/SurveyForm.tsx
@@ -0,0 +1,27 @@
+﻿import * as React from "react";
+import { Button, ButtonGroup, FormControl, InputGroup, Label } from "react-bootstrap";
+
+interface ISurveyFormProps {
+
+}
+
+interface ISurveyFormState {
+
+}
+
+export class SurveyForm extends React.Component<ISurveyFormProps, ISurveyFormState> {
+    render() {
+        return <div className='panel panel-primary dialog-panel'>
+            <div className='panel-heading'>
+                <h5>NetLS Software Development</h5>
+            </div>
+            <div className='panel-body'>
+                <form className='form-horizontal' role='form'>
+                    <div className="form-group">
+
+                    </div>
+                </form>
+            </div>
+        </div>;
+    }
+}
